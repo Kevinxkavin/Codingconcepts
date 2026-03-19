f6eceb5445779a91f5c39b6b4cfc58edd3b36254
Kavinkumar N <168163280+Kevinxkavin@users.noreply.github.com>
2026-03-19 09:36:52 +0530

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,1 +1,32 @@
+Stack (LIFO)
 
+👉 Last In, First Out
+
+🧠 Idea:
+
+Like a plate stack → last plate goes first
+
+✅ Operations:
+
+push() → add
+
+pop() → remove top
+
+peek() → see top
+
+💻 Java Code:
+
+import java.util.Stack;
+import java.util.LinkedList;
+public class Main
+{
+	public static void main(String[] args) {
+	    Stack<Integer> st = new Stack<>();
+	    st.push(10);
+	    st.push(20);
+	    System.out.println(st.pop());
+	    System.out.println(st.peek());
+	    
+		
+	}
+}
