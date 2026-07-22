ec8028357e925a6c52c9b0e3fc7213dc92255b3a
Kavinkumar N <168163280+Kevinxkavin@users.noreply.github.com>
2026-07-22 09:33:31 +0530

    Create countwords.java

---
+++ countwords.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+       String str = "I Love Java";
+       String[] words = str.split(" ");
+       System.out.println(words.length);
+}
+} ---- using split()
