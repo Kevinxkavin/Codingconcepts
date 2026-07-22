7d4316eb206f19f741b64f8372e593241b95fa12
Kavinkumar N <168163280+Kevinxkavin@users.noreply.github.com>
2026-07-22 09:30:22 +0530

    Create Split().java

---
+++ Split().java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+       String str = "Java Python C++";
+       String[] arr = str.split(" ");
+       for(String s : arr) {
+       System.out.println(s);
+    }
+}
+}
+}
